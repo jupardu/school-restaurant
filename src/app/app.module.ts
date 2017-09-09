803c0ff54b077251bb6bc6eef82590c6854bd5fc
jupardu <juampa7_@hotmail.com>
2017-09-09 16:40:43 -0500

    Menu Implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent} from './customers/index/index.component';
+import { AddComponent as OrderAddComponent } from './orders/add/add.component';
+
+const appRoutes: Routes = [
+  {path: 'customers/index', component: IndexComponent},
+  {path: 'orders/add', component: OrderAddComponent},
+];
+
 import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -15,8 +25,12 @@ import { CurrentOptionComponent } from './current-option/current-option.componen
   ],
   imports: [
     BrowserModule,
-    CustomersModule
-  ],
+    CustomersModule,
+    OrdersModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+],
   providers: [],
   bootstrap: [AppComponent]
 })
